dc6e22d7ea3c865e0ede104f8d3b2650f9695bdb
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-12 16:01:21 +0500

    feat: deleting Cart Items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -50,6 +50,9 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export const getCart = (state) => state.cart.cart;
 
 export default cartSlice.reducer;
